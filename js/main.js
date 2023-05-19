0f7280c82c0aa196e2af6326c49e6ab13a032a4c
Wakkaer2 <126867269+Wakkaer2@users.noreply.github.com>
2023-05-19 18:05:22 +0900

    a

--- js/main.js
+++ js/main.js
@@ -1,21 +1,5 @@
 (function ($) {
     "use strict";
-
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner();
-    
-    
-    // Initiate the wowjs
-    new WOW().init();
-
-
     // Sticky Navbar
     $(window).scroll(function () {
         if ($(this).scrollTop() > 300) {
@@ -24,6 +8,14 @@
             $('.sticky-top').css('top', '-100px');
         }
     });
+
+    // Spinner
+    var spinner = function () {
+    setTimeout(function () {
+    if ($('#spinner').length > 0) {
+      $('#spinner').removeClass('show');
+    }}, 1);};
+    spinner();
     
     
     // Dropdown on mouse hover
@@ -106,4 +98,3 @@
     });
     
 })(jQuery);
-
